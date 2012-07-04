e79195a1fe1a5ba104351a3c1624b68c35b55d89
James Rickard <james@frodosghost.com>
2012-07-04 23:40:40 +1000

    Added menu listener to build News items to display in list.

---
+++ EventListener/ConfigureMenuListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AGB\Bundle\NewsBundle\EventListener;
+
+use Manhattan\Bundle\ConsoleBundle\Event\ConfigureMenuEvent;
+
+class ConfigureMenuListener
+{
+    /**
+     * @param AGB\Bundle\ConsoleBundle\Event\ConfigureMenuEvent $event
+     */
+    public function onMenuConfigure(ConfigureMenuEvent $event)
+    {
+        $menu = $event->getMenu();
+
+        $dropdown = $menu->addChild('News', array('route'=>'console_news'))
+            ->setLinkattribute('class', 'dropdown-toggle')
+            ->setLinkattribute('data-toggle', 'dropdown')
+            ->setAttribute('class', 'dropdown')
+            ->setChildrenAttribute('class', 'menu-dropdown');
+
+        $dropdown->addChild('News', array('route' => 'console_news'));
+        $dropdown->addChild('New News Post', array('route' => 'console_news_new'));
+
+    }
+}
